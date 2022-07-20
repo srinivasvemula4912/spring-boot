7990ac14a322184127f6c5f6889e52e46e968d23
Srinivas Vemula <srinivas4912@gmail.com>
2022-07-20 18:06:54 +0530

    Revert "comming new projects"
    This reverts commit 42f4878209071378ef4deed81d9cd18659c3dac0.

--- order-service/order-service/src/main/java/com/vl/orderservice/api/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vl.orderservice.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vl.orderservice.api.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer>{
-
-}
--- payment-service/payment-service/src/main/java/com/vl/paymentservice/api/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vl.paymentservice.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vl.paymentservice.api.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer>{
-
-}
